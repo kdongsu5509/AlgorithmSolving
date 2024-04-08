bde9ac736c3609705876c48b47b90fffde854d8c
Dongsu Ko <127917642+kdongsu5509@users.noreply.github.com>
2024-04-09 00:57:59 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- Python3/프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ Python3/프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,23 +1,17 @@
-def valid(s):
-    stack = []
-    for temp in s:
-        if temp == '(' or temp == '{' or temp == '[':
-            stack.append(temp)
-        elif temp == ')':
-            if not stack or stack.pop() != '(':
-                return False
-        elif temp == '}':
-            if not stack or stack.pop() != '{':
-                return False
-        elif temp == ']':
-            if not stack or stack.pop() != '[':
-                return False
-    return not stack
+from collections import deque
+
+def check(s):
+    while True:
+        if "()" in s: s=s.replace("()","")
+        elif "{}" in s: s=s.replace("{}","")
+        elif "[]" in s: s=s.replace("[]","")
+        else: return False if s else True       
 
 def solution(s):
-    cnt = 0
+    ans = 0
+    que = deque(s)
+
     for i in range(len(s)):
-        temp = s[i:] + s[:i]
-        if valid(temp):
-            cnt += 1
-    return cnt
+        if check(''.join(que)): ans+=1
+        que.rotate(-1)
+    return ans
