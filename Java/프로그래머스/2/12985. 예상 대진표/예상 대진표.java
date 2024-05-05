76e1a1b1346f9fb1f67a1e0d823cef5322b66704
Dongsu Ko <127917642+kdongsu5509@users.noreply.github.com>
2024-05-05 20:09:55 +0900

    [level 2] Title: 예상 대진표, Time: 0.23 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int cnt = 0;
+        while(true){
+            a = (int)Math.ceil((double)a / 2);
+            b = (int)Math.ceil((double)b / 2);
+            
+            cnt++;
+            if(a == b){
+                return cnt;
+            }
+        }
+    }
+}
