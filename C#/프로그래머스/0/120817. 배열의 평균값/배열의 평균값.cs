eee7b86efdf55e431e79aacff706853c7737001e
Dongsu Ko <127917642+kdongsu5509@users.noreply.github.com>
2024-05-12 12:19:58 +0900

    [level 0] Title: 배열의 평균값, Time: 0.81 ms, Memory: 31.3 MB -BaekjoonHub

--- C#/프로그래머스/0/120817. 배열의 평균값/배열의 평균값.cs
+++ C#/프로그래머스/0/120817. 배열의 평균값/배열의 평균값.cs
@@ -1,11 +1,8 @@
 using System;
+using System.Linq;
 
 public class Solution {
     public double solution(int[] numbers) {
-        double answer = 0;
-        foreach(int temp in numbers){
-            answer += temp;
-        }
-        return answer / numbers.Length;
+        return numbers.Average();
     }
 }
