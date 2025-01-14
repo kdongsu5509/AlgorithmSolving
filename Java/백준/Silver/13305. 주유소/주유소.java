0c05dee7d245dd7f8d1674e854621486c517826d
dongsu_ko <127917642+kdongsu5509@users.noreply.github.com>
2025-01-14 10:23:44 +0900

    [Silver III] Title: 주유소, Time: 452 ms, Memory: 53044 KB, Score: 100 point -BaekjoonHub

--- Java/백준/Silver/13305. 주유소/주유소.java
+++ Java/백준/Silver/13305. 주유소/주유소.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
@@ -6,31 +7,29 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int cityCnt = Integer.parseInt(br.readLine().trim());
-        int[] loadLength = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int[] oilMoney = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        long[] loadLength = Arrays.stream(br.readLine().split(" ")).mapToLong(Integer::parseInt).toArray();
+        long[] oilMoney = Arrays.stream(br.readLine().split(" ")).mapToLong(Integer::parseInt).toArray();
 
-        int nowPrice = Integer.MAX_VALUE;
-        int prevPrice = Integer.MAX_VALUE;
+        long nowPrice = Integer.MAX_VALUE;
+        long prevPrice = Integer.MAX_VALUE;
 
         int nowCursor = 0;
         int prevCursor = 0;
 
-        int totalPrice = 0;
+        BigInteger totalPrice = BigInteger.valueOf(0L);
 
         for (int i = 0; i < cityCnt - 1; i++) {
             nowCursor = i;
             nowPrice = oilMoney[i];
 
             if (nowPrice < prevPrice) {
-                totalPrice += (nowPrice * loadLength[i]);
+                totalPrice = totalPrice.add(BigInteger.valueOf(nowPrice * loadLength[i]));
                 prevCursor = nowCursor;
                 prevPrice = nowPrice;
             } else {
-                totalPrice += (prevPrice * loadLength[i]);
+                totalPrice = totalPrice.add(BigInteger.valueOf(prevPrice * loadLength[i]));
             }
         }
-
         System.out.println(totalPrice);
-
     }
 }
