58de1fc87c2c3e039a31c308de304b497f65e227
dongsu_ko <127917642+kdongsu5509@users.noreply.github.com>
2025-02-02 17:11:04 +0900

    [Gold V] Title: 용액, Time: 388 ms, Memory: 36224 KB -BaekjoonHub

---
+++ Java/백준/Gold/2467. 용액/용액.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int cnt = Integer.parseInt(br.readLine());
+
+        List<Long> numbers = new ArrayList<>();
+
+        Arrays.stream(br.readLine().split(" ")).mapToLong(Long::parseLong).forEach(numbers::add);
+
+        int left = 0;
+        int right = numbers.size() - 1;
+
+        List<Long> candidatesNumbers = new ArrayList<>();
+        Long candiatesSum = Long.MAX_VALUE;
+        while (left < right) {
+            Long tempL = numbers.get(left);
+            Long tempR = numbers.get(right);
+            Long temp = tempL + tempR;
+
+            if (temp <= 0) {
+                left++;
+            } else {
+                right--;
+            }
+
+            if (Math.abs(temp) <= Math.abs(candiatesSum)) {
+                candiatesSum = temp;
+                candidatesNumbers.clear();
+                addTocandidatesNumbers(candidatesNumbers, tempL, tempR);
+            }
+
+        }
+
+        // Collections.sort(candidatesNumbers);
+        bw.append(candidatesNumbers.get(0) + " " + candidatesNumbers.get(1));
+        bw.flush();
+    }
+
+    private static void addTocandidatesNumbers(List<Long> candidatesNumbers, Long tempL, Long tempR) {
+        candidatesNumbers.add(tempL);
+        candidatesNumbers.add(tempR);
+    }
+}
